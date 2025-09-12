0563ffe355c2b1270a1b416998763e1586628400
Nitin Rao <nitinsprao@gmail.com>
2025-09-11 22:22:33 -0400

    Implement signal handling and actual shell functionality

--- part1/shell.c
+++ part1/shell.c
@@ -1,3 +1,5 @@
+#define _POSIX_C_SOURCE 200809L
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,14 +8,9 @@
 #include <unistd.h>
 #include <errno.h>
 #include <signal.h>
+#include <signal.h> // Ensure struct sigaction is defined
 
 #define MAX_ARGS 128
-static void die(const char *s)
-{
-    perror(s);
-    exit(1);
-}
-
 static int tokenize(char *line, char *argv[], int max)
 {
     int argc = 0;
@@ -42,13 +39,62 @@ static int tokenize(char *line, char *argv[], int max)
     return argc;
 }
 
+static int handle_builtin(char *argv[])
+{
+    if (argv[0] == NULL) return 1;
+
+    if (strcmp(argv[0], "exit") == 0) {
+        return 0;
+    }
+
+    if(strcmp(argv[0], "cd") == 0) {
+        if (argv[1] == NULL || argv[2] != NULL) {
+            fprintf(stderr, "error: %s\n", "usage: cd <dir>");
+        } else if (chdir(argv[1]) == -1) {
+            fprintf(stderr, "error: %s\n", strerror(errno));
+        }
+        return 1;
+    }
+    
+    return 2;
+}
+
+static void install_signal_handlers(void) {
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = SIG_IGN;
+    sigaction(SIGINT, &sa, NULL);
+}
+
+static void run_external(char *argv[]) {
+    pid_t pid = fork();
+    if (pid < 0) {
+        fprintf(stderr, "error: %s\n", strerror(errno));
+        return;
+    } else if (pid == 0) {
+        struct sigaction sa_ch;
+        memset(&sa_ch, 0, sizeof(sa_ch));
+        sa_ch.sa_handler = SIG_DFL;
+        sigaction(SIGINT, &sa_ch, NULL);
+
+        execv(argv[0], argv);
+        fprintf(stderr, "error: %s\n", strerror(errno));
+        _exit(127);
+    } else {
+        int status;
+        if (waitpid(pid, &status, 0) == -1) {
+            fprintf(stderr, "error: %s\n", strerror(errno));
+        }
+    }
+}
+
 int main(void)
 {
+    install_signal_handlers();
+
     char   *line = NULL;
     size_t  cap  = 0;
     ssize_t nread;
-    pid_t   pid;
-    int     status;
 
     printf("$");
     fflush(stdout);
@@ -73,20 +119,16 @@ int main(void)
             continue;
         }
 
-        pid = fork();
-        if (pid < 0) {
-            die("fork error");
-        } else if (pid == 0) {
-            /* child process */
-            execl(line, line, (char *)0);
-            /* if execl returns, it failed */
-            fprintf(stderr, "error: %s\n", strerror(errno));
-            _exit(127);
-        } else {
-            /* parent process */
-            if (waitpid(pid, &status, 0) != pid)
-                die("waitpid failed");
+        int builtin = handle_builtin(argv);
+        if (builtin == 0) {
+            break;
+        } else if (builtin == 1) {
+            printf("$");
+            fflush(stdout);
+            continue;
         }
+
+        run_external(argv);
 
         printf("$");
         fflush(stdout);
